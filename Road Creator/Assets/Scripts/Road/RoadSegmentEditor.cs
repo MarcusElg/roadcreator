5bbb7ddb87aec674a89771aa2a0f65eb22ed7207
MCrafterzz <marcusaccounts@yahoo.se>
2018-06-14 12:52:38 +0200

    Added multiedit support for segments

--- Road Creator/Assets/Scripts/Road/RoadSegmentEditor.cs
+++ Road Creator/Assets/Scripts/Road/RoadSegmentEditor.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(RoadSegment))]
-public class RoadSegmentEditor : Editor {
+[CanEditMultipleObjects]
+public class RoadSegmentEditor : Editor
+{
 
     public RoadSegment segment;
 
@@ -16,35 +18,36 @@ public class RoadSegmentEditor : Editor {
     public override void OnInspectorGUI()
     {
         EditorGUI.BeginChangeCheck();
-        segment.roadMaterial = (Material)EditorGUILayout.ObjectField("Road Material", segment.roadMaterial, typeof(Material), false);
-        segment.roadWidth = Mathf.Max(0.1f, EditorGUILayout.FloatField("Road Width", segment.roadWidth));
-        segment.flipped = EditorGUILayout.Toggle("Road Flipped", segment.flipped);
+        serializedObject.FindProperty("roadMaterial").objectReferenceValue = (Material)EditorGUILayout.ObjectField("Road Material", serializedObject.FindProperty("roadMaterial").objectReferenceValue, typeof(Material), false);
+        serializedObject.FindProperty("roadWidth").floatValue = Mathf.Max(0.1f, EditorGUILayout.FloatField("Road Width", serializedObject.FindProperty("roadWidth").floatValue));
+        serializedObject.FindProperty("flipped").boolValue = EditorGUILayout.Toggle("Road Flipped", serializedObject.FindProperty("flipped").boolValue);
 
         GUIStyle guiStyle = new GUIStyle();
         guiStyle.fontStyle = FontStyle.Bold;
 
         GUILayout.Label("");
         GUILayout.Label("Left Shoulder", guiStyle);
-        segment.leftShoulder = EditorGUILayout.Toggle("Left Shoulder", segment.leftShoulder);
-        if (segment.leftShoulder == true)
+        serializedObject.FindProperty("leftShoulder").boolValue = EditorGUILayout.Toggle("Left Shoulder", serializedObject.FindProperty("leftShoulder").boolValue);
+        if (serializedObject.FindProperty("leftShoulder").boolValue == true)
         {
-            segment.leftShoulderMaterial = (Material)EditorGUILayout.ObjectField("Left Shoulder Material", segment.leftShoulderMaterial, typeof(Material), false);
-            segment.leftShoulderWidth = Mathf.Max(0.1f, EditorGUILayout.FloatField("Left Shoulder Width", segment.leftShoulderWidth));
-            segment.leftShoulderHeightOffset = EditorGUILayout.FloatField("Left Shoulder Y Offset", segment.leftShoulderHeightOffset);
+            serializedObject.FindProperty("leftShoulderMaterial").objectReferenceValue = (Material)EditorGUILayout.ObjectField("Left Shoulder Material", serializedObject.FindProperty("leftShoulderMaterial").objectReferenceValue, typeof(Material), false);
+            serializedObject.FindProperty("leftShoulderWidth").floatValue = Mathf.Max(0.1f, EditorGUILayout.FloatField("Left Shoulder Width", serializedObject.FindProperty("leftShoulderWidth").floatValue));
+            serializedObject.FindProperty("leftShoulderHeightOffset").floatValue = EditorGUILayout.FloatField("Left Shoulder Y Offset", serializedObject.FindProperty("leftShoulderHeightOffset").floatValue);
         }
 
         GUILayout.Label("");
         GUILayout.Label("Right Shoulder", guiStyle);
-        segment.rightShoulder = EditorGUILayout.Toggle("Right Shoulder", segment.rightShoulder);
+        serializedObject.FindProperty("rightShoulder").boolValue = EditorGUILayout.Toggle("Right Shoulder", serializedObject.FindProperty("rightShoulder").boolValue);
         if (segment.rightShoulder == true)
         {
-            segment.rightShoulderMaterial = (Material)EditorGUILayout.ObjectField("Right Shoulder Material", segment.rightShoulderMaterial, typeof(Material), false);
-            segment.rightShoulderWidth = Mathf.Max(0.1f, EditorGUILayout.FloatField("Right Shoulder Width", segment.rightShoulderWidth));
-            segment.rightShoulderHeightOffset = EditorGUILayout.FloatField("Right Shoulder Y Offset", segment.rightShoulderHeightOffset);
+            serializedObject.FindProperty("rightShoulderMaterial").objectReferenceValue = (Material)EditorGUILayout.ObjectField("Right Shoulder Material", serializedObject.FindProperty("rightShoulderMaterial").objectReferenceValue, typeof(Material), false);
+            serializedObject.FindProperty("rightShoulderWidth").floatValue = Mathf.Max(0.1f, EditorGUILayout.FloatField("Right Shoulder Width", serializedObject.FindProperty("rightShoulderWidth").floatValue));
+            serializedObject.FindProperty("rightShoulderHeightOffset").floatValue = EditorGUILayout.FloatField("Right Shoulder Y Offset", serializedObject.FindProperty("rightShoulderHeightOffset").floatValue);
         }
 
         if (EditorGUI.EndChangeCheck())
         {
+            serializedObject.ApplyModifiedProperties();
             segment.transform.parent.parent.GetComponent<RoadCreator>().roadEditor.UpdateMesh();
         }
     }
