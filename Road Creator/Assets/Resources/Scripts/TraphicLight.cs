b67b28080165741a567bbf190df747c40f22eb1b
MCrafterzz <marcusaccounts@yahoo.se>
2018-08-21 08:37:07 +0200

    Added functional traphic lights

---
+++ Road Creator/Assets/Resources/Scripts/TraphicLight.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TraphicLight : MonoBehaviour
+{
+
+    public Material greenActive;
+    public Material greenNonActive;
+    public Material yellowActive;
+    public Material yellowNonActive;
+    public Material redActive;
+    public Material redNonActive;
+
+    public float greenTime = 30;
+    public float yellowBeforeRedTime = 5;
+    public float redTime = 60;
+    public float yellowBeforeGreenTime = 1;
+    public float startTime = 0;
+
+    public float timeSinceLast = 0;
+    public float nextChangeTick = 0;
+
+    public enum TraphicColour { Green, YellowBeforeRed, Red, YellowBeforeGreen };
+    public TraphicColour currentColour;
+    public TraphicColour startColour;
+
+    public bool paused = false;
+
+    public void Start()
+    {
+        ModifyChangeTick();
+    }
+
+    public void Update()
+    {
+        if (paused == false)
+        {
+            timeSinceLast += Time.deltaTime;
+
+            if (timeSinceLast >= nextChangeTick)
+            {
+                if (currentColour == TraphicColour.Green)
+                {
+                    currentColour = TraphicColour.YellowBeforeRed;
+                }
+                else if (currentColour == TraphicColour.YellowBeforeRed)
+                {
+                    currentColour = TraphicColour.Red;
+                }
+                else if (currentColour == TraphicColour.Red)
+                {
+                    currentColour = TraphicColour.YellowBeforeGreen;
+                }
+                else if (currentColour == TraphicColour.YellowBeforeGreen)
+                {
+                    currentColour = TraphicColour.Green;
+                }
+
+                timeSinceLast = 0;
+                ModifyChangeTick();
+                UpdateMaterials();
+            }
+        }
+    }
+
+    public void ModifyChangeTick()
+    {
+        if (currentColour == TraphicColour.Green)
+        {
+            nextChangeTick = greenTime;
+        }
+        else if (currentColour == TraphicColour.YellowBeforeRed)
+        {
+            nextChangeTick = yellowBeforeRedTime;
+        }
+        else if (currentColour == TraphicColour.Red)
+        {
+            nextChangeTick = redTime;
+        }
+        else if (currentColour == TraphicColour.YellowBeforeGreen)
+        {
+            nextChangeTick = yellowBeforeGreenTime;
+        }
+    }
+
+    public void UpdateMaterials()
+    {
+        Material[] materials = transform.GetComponent<MeshRenderer>().sharedMaterials;
+        materials[4] = greenNonActive;
+        materials[3] = yellowNonActive;
+        materials[2] = redNonActive;
+
+        if (currentColour == TraphicColour.Green)
+        {
+            materials[4] = greenActive;
+        }
+        else
+        if (currentColour == TraphicColour.YellowBeforeRed || currentColour == TraphicColour.YellowBeforeGreen)
+        {
+            materials[3] = yellowActive;
+        }
+        else
+        if (currentColour == TraphicColour.Red)
+        {
+            materials[2] = redActive;
+        }
+
+        transform.GetComponent<MeshRenderer>().sharedMaterials = materials;
+    }
+
+}
---
+++ Road Creator/Assets/Resources/Scripts/TraphicLightEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TraphicLight))]
+public class TraphicLightEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+        GUIStyle guiStyle = new GUIStyle();
+        guiStyle.fontStyle = FontStyle.Bold;
+
+        GUILayout.Label("Materials", guiStyle);
+        serializedObject.FindProperty("greenActive").objectReferenceValue = (Material)EditorGUILayout.ObjectField("Green Active Material", serializedObject.FindProperty("greenActive").objectReferenceValue, typeof(Material), false);
+        serializedObject.FindProperty("greenNonActive").objectReferenceValue = (Material)EditorGUILayout.ObjectField("Green Non-active Material", serializedObject.FindProperty("greenNonActive").objectReferenceValue, typeof(Material), false);
+        serializedObject.FindProperty("yellowActive").objectReferenceValue = (Material)EditorGUILayout.ObjectField("Yellow Active Material", serializedObject.FindProperty("yellowActive").objectReferenceValue, typeof(Material), false);
+        serializedObject.FindProperty("yellowNonActive").objectReferenceValue = (Material)EditorGUILayout.ObjectField("Yellow Non-active Material", serializedObject.FindProperty("yellowNonActive").objectReferenceValue, typeof(Material), false);
+        serializedObject.FindProperty("redActive").objectReferenceValue = (Material)EditorGUILayout.ObjectField("Red Active Material", serializedObject.FindProperty("redActive").objectReferenceValue, typeof(Material), false);
+        serializedObject.FindProperty("redNonActive").objectReferenceValue = (Material)EditorGUILayout.ObjectField("Red Non-active Material", serializedObject.FindProperty("redNonActive").objectReferenceValue, typeof(Material), false);
+
+        GUILayout.Label("");
+        GUILayout.Label("Timing", guiStyle);
+
+        serializedObject.FindProperty("greenTime").floatValue = Mathf.Max(0, EditorGUILayout.FloatField("Green Time", serializedObject.FindProperty("greenTime").floatValue));
+        serializedObject.FindProperty("yellowBeforeRedTime").floatValue = Mathf.Max(0, EditorGUILayout.FloatField("Yellow Before Red Time", serializedObject.FindProperty("yellowBeforeRedTime").floatValue));
+        serializedObject.FindProperty("redTime").floatValue = Mathf.Max(0, EditorGUILayout.FloatField("Red Time", serializedObject.FindProperty("redTime").floatValue));
+        serializedObject.FindProperty("yellowBeforeGreenTime").floatValue = Mathf.Max(0, EditorGUILayout.FloatField("Yellow Before Green Time", serializedObject.FindProperty("yellowBeforeGreenTime").floatValue));
+        serializedObject.FindProperty("startColour").enumValueIndex = (int)(TraphicLight.TraphicColour)EditorGUILayout.EnumPopup("Start Colour", (TraphicLight.TraphicColour)System.Enum.GetValues(typeof(TraphicLight.TraphicColour)).GetValue(serializedObject.FindProperty("startColour").enumValueIndex));
+
+        if (EditorGUI.EndChangeCheck() == true)
+        {
+            serializedObject.FindProperty("currentColour").enumValueIndex = serializedObject.FindProperty("startColour").enumValueIndex;
+            serializedObject.ApplyModifiedProperties();
+            
+            for (int i = 0; i < targets.Length; i++)
+            {
+                ((TraphicLight)targets[i]).UpdateMaterials();
+            }
+        }
+    }
+
+}
